6dab1313b4bd899d1e8619ea233f6d993afdcf4b
adamufura <adamufura98@gmail.com>
2025-08-23 00:39:17 +0100

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    historyApiFallback: true
-  }
-}) 
+  },
+  preview: {
+    port: 4173,
+  },
+  build: {
+    outDir: "dist",
+  },
+  base: "/",
+});
