09dcaa8971ef05588f2eda1f67a1c424ee546c77
adamufura <adamufura98@gmail.com>
2025-08-24 15:26:48 +0100

    vite fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,22 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 👇 makes sure assets (images, css, js) use relative paths
+  base: "./",
+
   server: {
     port: 3000,
+    host: true, // 👈 ensures it works in docker/easypanel/remote
     open: true,
   },
+
   preview: {
     port: 4173,
+    host: true, // 👈 same fix for preview build
+    open: false,
   },
+
   build: {
     outDir: "dist",
   },
